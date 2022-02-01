ef66e177d48ffdad90c52c9b7ec9f6b0a9500cb6
mrmuddd <90551962+mrmuddd@users.noreply.github.com>
2022-02-01 07:28:48 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,27 +2,40 @@ from random import choice
 from glob import glob
 import telebot
 from telebot import types
-
+import requests as r
 
 KEY = '5126890620:AAEw-CztNB-4rldEnhO9MVrUfXafHZvSWVQ'
+NEWS_KEY = 'c86169d8dfb14848a46619b9f6d4006a'
+
 bot = telebot.TeleBot(KEY)
 
 
 @bot.message_handler(commands=['start'])                                         # the main menu
 def start(msg):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton('Пополнить словарный запас')
-    item2 = types.KeyboardButton('Показать хряка')
-    item3 = types.KeyboardButton('Посмотреть на панков')
+    item1 = types.KeyboardButton('Покажи новости')
+    item2 = types.KeyboardButton('Пополнить словарный запас')
+    item3 = types.KeyboardButton('Покажи хряка')
+    item4 = types.KeyboardButton('Покажи панков')
     markup.add(item1)
     markup.add(item2)
     markup.add(item3)
+    markup.add(item4)
     bot.send_message(msg.chat.id, 'Что хочешь?', reply_markup=markup)
 
 
-@bot.message_handler(content_types='text')                                        # enrichment
-def enrichment(msg):
-    if msg.text == 'Пополнить словарный запас':
+@bot.message_handler(content_types='text')                                               # news
+def buttons(msg):
+    if msg.text == 'Покажи новости':
+        message = ''
+        url = 'https://newsapi.org/v2/top-headlines?country=ru&apiKey=c86169d8dfb14848a46619b9f6d4006a'
+        response = r.get(url)
+        data = response.json()
+        for i in range(5):
+            message += format(str(data['articles'][i]['title']) + '\n' + '\n')
+        bot.reply_to(msg, message)
+
+    if msg.text == 'Пополнить словарный запас':                                       # enrichment
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         item1 = types.KeyboardButton('A snapchat pic')
         item2 = types.KeyboardButton('Quaaludes')
@@ -37,7 +50,7 @@ def enrichment(msg):
     elif msg.text == 'Quaaludes':
         bot.send_message(msg.chat.id, 'https://youtu.be/IG2JF0P4GFA')
 
-    if msg.text == 'Посмотреть на панков':                                                 # punks
+    if msg.text == 'Покажи панков':                                                 # punks
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         item1 = types.KeyboardButton('Услада для ушей')
         item2 = types.KeyboardButton('Крутая репетиция')
@@ -64,7 +77,7 @@ def enrichment(msg):
     elif msg.text == 'Замерзшие пельмени куда-то едут под дикие оры панков':
         bot.send_message(msg.chat.id, 'https://vm.tiktok.com/ZSegm63pF/')
 
-    if msg.text == 'Показать хряка':                                                          # PIGGIE
+    if msg.text == 'Покажи хряка':                                                          # PIGGIE
         pigs = glob('imgs/*')
         pig = choice(pigs)
         bot.send_photo(msg.chat.id, open(pig, 'rb'))
