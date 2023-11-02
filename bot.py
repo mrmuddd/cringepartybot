36477b0e6092fe25ca94ff138f2de9d3e8aaac55
mrmuddd <90551962+mrmuddd@users.noreply.github.com>
2023-11-03 03:35:25 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,12 +99,12 @@ def actions(msg):
         aurora = r.get('https://services.swpc.noaa.gov/images/aurora-forecast-northern-hemisphere.jpg').content
         bot.send_photo(msg.chat.id, aurora, reply_to_message_id=msg.message_id,
                        caption='Вероятность сияния - прогноз на 30 минут')
-        message = bot.send_message(msg.chat.id, 'Загрузка видео: 0/2')
-        soho_c3_data = r.get('https://soho.nascom.nasa.gov/data/LATEST/current_c3small.mp4', stream=True).content
-        bot.edit_message_text('Загрузка видео: 1/2', msg.chat.id, message.message_id)
+        message = bot.send_message(msg.chat.id, 'Загрузка видео: 0/1')
+        #soho_c3_data = r.get('https://soho.nascom.nasa.gov/data/LATEST/current_c3small.mp4', stream=True).content
+        #bot.edit_message_text('Загрузка видео: 1/2', msg.chat.id, message.message_id)
         soho_c2_data = r.get('https://soho.nascom.nasa.gov/data/LATEST/current_c2small.mp4', stream=True).content
-        bot.edit_message_text('Загрузка видео: 2/2', msg.chat.id, message.message_id)
-        bot.send_video(msg.chat.id, soho_c3_data, caption='LASCO C3')
+        bot.edit_message_text('Загрузка видео: 1/1', msg.chat.id, message.message_id)
+        #bot.send_video(msg.chat.id, soho_c3_data, caption='LASCO C3')
         bot.send_video(msg.chat.id, soho_c2_data, caption='LASCO C2')
         bot.delete_message(msg.chat.id, message.message_id)
 
