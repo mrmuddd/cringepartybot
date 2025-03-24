b5c2cfd558f7226842656c1160e6536b7fbfe238
mrmuddd <90551962+mrmuddd@users.noreply.github.com>
2025-03-24 21:45:47 +0700

    Update bot.py
    deleted news, fixed sun and earth

--- bot.py
+++ bot.py
@@ -5,73 +5,76 @@ from telebot import types
 import requests as r
 from bs4 import BeautifulSoup as bs
 
-
 KEY = '5126890620:AAEw-CztNB-4rldEnhO9MVrUfXafHZvSWVQ'
 
 bot = telebot.TeleBot(KEY)
 
-buttons = ['Обновить', 'Небо над головой 🔭', 'Погода 🌦', 'Курсы валют 😭', 'Новости 💀', 'ХРЯК 🐖', 'КАБАН 🦍']
+buttons = ['Обновить бота', 'Небо над головой 🔭', 'Погода 🌦', 'Курсы валют 😭', 'ХРЯК 🐖', 'КАБАН 🦍']
+
+
 @bot.message_handler(commands=['start'])
 def start(msg):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.row('Обновить')
+    markup.row('Обновить бота')
+    markup.row('ХРЯК 🐖', 'КАБАН 🦍')
     markup.row(types.KeyboardButton('Небо 🔭', request_location=True), 'Погода 🌦')
     markup.row('Земля 🌎', 'Солнце ☀')
-    markup.row('Курсы валют 😭', 'Новости 💀')
-    markup.row('ХРЯК 🐖', 'КАБАН 🦍')
+    markup.row('Курсы валют 😭')
 
     bot.send_message(msg.chat.id, 'Что хочешь?', reply_markup=markup)
 
 
-@bot.message_handler(content_types=['text','location','InputMediaPhoto','animation'])
+@bot.message_handler(content_types=['text', 'location', 'InputMediaPhoto', 'animation'])
 def actions(msg):
-    if msg.text == 'Обновить':
+    if msg.text == 'Обновить бота':
         start(msg)
 
     # weather
 
     if msg.text == 'Погода 🌦':
         bot.reply_to(msg, 'Напиши название населенного пункта')
+
         @bot.message_handler(content_types=['text'])
         def weather(msg):
-                coord_url = f"http://api.openweathermap.org/geo/1.0/direct?q={str(msg.text)}&limit=5&appid=2dd9ea7ad178166dee8752723832cd70"
-                coord_data = r.get(coord_url).json()
-                if coord_data != []:
-                    lat = f"{coord_data[0]['lat']}"
-                    lon = f"{coord_data[0]['lon']}"
-                    weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&lang=ru&appid=2dd9ea7ad178166dee8752723832cd70"
-                    weather_data = r.get(weather_url).json()
+            coord_url = f"http://api.openweathermap.org/geo/1.0/direct?q={str(msg.text)}&limit=5&appid=2dd9ea7ad178166dee8752723832cd70"
+            coord_data = r.get(coord_url).json()
+            if coord_data != []:
+                lat = f"{coord_data[0]['lat']}"
+                lon = f"{coord_data[0]['lon']}"
+                weather_url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&lang=ru&appid=2dd9ea7ad178166dee8752723832cd70"
+                weather_data = r.get(weather_url).json()
 
-                    placeURL = f"https://www.geonames.org/search.html?q={str(msg.text)}"
-                    soup = bs(r.get(placeURL).text, 'html.parser')
-                    ids = [link['href'] for link in soup.find_all('a', href=True)]
-                    ID = ids[6][1:8]
+                placeURL = f"https://www.geonames.org/search.html?q={str(msg.text)}"
+                soup = bs(r.get(placeURL).text, 'html.parser')
+                ids = [link['href'] for link in soup.find_all('a', href=True)]
+                ID = ids[6][1:8]
 
-                    astroURL = f"https://www.meteoblue.com/en/weather/outdoorsports/seeing/" \
-                               f"{coord_data[0]['name'].replace(' ', '-')}_" \
-                               f"{coord_data[0]['country']}_{ID}"
-                    astro_message = 'Астрономическая видимость:' + '\n' + astroURL
+                astroURL = f"https://www.meteoblue.com/en/weather/outdoorsports/seeing/" \
+                           f"{coord_data[0]['name'].replace(' ', '-')}_" \
+                           f"{coord_data[0]['country']}_{ID}"
+                astro_message = 'Астрономическая видимость:' + '\n' + astroURL
 
-                    weather_message = f"Сейчас на улице:" + '\n' * 2 \
-                                      + f"{weather_data['weather'][0]['description']}" + '\n' \
-                                      + f"Температура: {weather_data['main']['temp']}" + '\n' \
-                                      + f"Ощущается как: {weather_data['main']['feels_like']}" + '\n' \
-                                      + f"Мин: {weather_data['main']['temp_min']}" + '\n' \
-                                      + f"Макс: {weather_data['main']['temp_max']}" + '\n' \
-                                      + f"Влажность: {weather_data['main']['humidity']}" '\n'
+                weather_message = f"Сейчас на улице:" + '\n' * 2 \
+                                  + f"{weather_data['weather'][0]['description']}" + '\n' \
+                                  + f"Температура: {weather_data['main']['temp']}" + '\n' \
+                                  + f"Ощущается как: {weather_data['main']['feels_like']}" + '\n' \
+                                  + f"Мин: {weather_data['main']['temp_min']}" + '\n' \
+                                  + f"Макс: {weather_data['main']['temp_max']}" + '\n' \
+                                  + f"Влажность: {weather_data['main']['humidity']}" '\n'
 
-                    bot.send_location(msg.chat.id, float(lat), float(lon))
-                    bot.reply_to(msg, astro_message)
-                    bot.reply_to(msg, weather_message)
-                else:
-                    bot.reply_to(msg, 'Не знаю такого😭😭😭')
+                bot.send_location(msg.chat.id, float(lat), float(lon))
+                bot.reply_to(msg, astro_message)
+                bot.reply_to(msg, weather_message)
+            else:
+                bot.reply_to(msg, 'Не знаю такого😭😭😭')
 
         bot.register_next_step_handler(msg, weather)
 
-    #Earth pics
+    # Earth
 
     if msg.text == 'Земля 🌎':
-        url = 'https://api.nasa.gov/EPIC/api/natural/images?api_key=iCd9fDWK0WiK09yYma0dW5w1dNbRYjVaDx8Ho2bk'
+        nasakey = 'iCd9fDWK0WiK09yYma0dW5w1dNbRYjVaDx8Ho2bk'
+        url = f'https://api.nasa.gov/EPIC/api/natural/images?api_key={nasakey}'
         data = r.get(url).json()
         imgs = []
         dates = []
@@ -79,36 +82,42 @@ def actions(msg):
         message = bot.send_message(msg.chat.id, f'Загрузка изображений: {count}/{len(data)}')
         for i in data:
             dates.append(i['date'])
-            count+=1
-            img = r.get(f"https://epic.gsfc.nasa.gov./epic-archive/jpg/{i['image']}.jpg").content
+            count += 1
+            y = str(i['date'])[0:4]
+            m = str(i['date'])[5:7]
+            d = str(i['date'])[8:10]
+            print(y,m,d)
+            img = r.get(f"https://api.nasa.gov/EPIC/archive/natural/{y}/{m}/{d}/jpg/{i['image']}.jpg?api_key={nasakey}").content
+            print(img)
             imgs.append(types.InputMediaPhoto(img))
             bot.edit_message_text(f'Загрузка изображений: {count}/{len(data)}', msg.chat.id, message.message_id)
-            
+
         bot.delete_message(msg.chat.id, message.message_id)
         for i in range(len(dates)):
-            dates[i] = f"{i+1} снимок: "+str(dates[i])
+            dates[i] = f"{i + 1} снимок: " + str(dates[i])
         bot.send_media_group(msg.chat.id, imgs[0:10])
         if len(imgs) > 10:
             bot.send_media_group(msg.chat.id, imgs[10:len(imgs)])
         bot.send_message(msg.chat.id, 'NOAA DSCOVR - последние снимки' + '\n\n'
                          + 'Дата и время (YYYY-MM-DD, UTC+0): ' + f'\n\n' + '\n'.join(map(str, dates)))
 
-    #sun
+    # sun
 
     if msg.text == 'Солнце ☀':
         aurora = r.get('https://services.swpc.noaa.gov/images/aurora-forecast-northern-hemisphere.jpg').content
         bot.send_photo(msg.chat.id, aurora, reply_to_message_id=msg.message_id,
                        caption='Вероятность сияния - прогноз на 30 минут')
         message = bot.send_message(msg.chat.id, 'Загрузка видео: 0/1')
-        #soho_c3_data = r.get('https://soho.nascom.nasa.gov/data/LATEST/current_c3small.mp4', stream=True).content
-        #bot.edit_message_text('Загрузка видео: 1/2', msg.chat.id, message.message_id)
+        soho_c3_data = r.get('https://soho.nascom.nasa.gov/data/LATEST/current_c3small.mp4', stream=True).content
+        bot.edit_message_text('Загрузка видео: 1/2', msg.chat.id, message.message_id)
         soho_c2_data = r.get('https://soho.nascom.nasa.gov/data/LATEST/current_c2small.mp4', stream=True).content
         bot.edit_message_text('Загрузка видео: 1/1', msg.chat.id, message.message_id)
-        #bot.send_video(msg.chat.id, soho_c3_data, caption='LASCO C3')
+        bot.send_video(msg.chat.id, soho_c3_data, caption='LASCO C3')
         bot.send_video(msg.chat.id, soho_c2_data, caption='LASCO C2')
         bot.delete_message(msg.chat.id, message.message_id)
 
-    #sky
+
+    # sky
 
     if msg.location is not None:
         lat = msg.location.latitude
@@ -136,23 +145,12 @@ def actions(msg):
         dataEUR = r.get(EUR).json()
         dataUSD = r.get(USD).json()
         dataRUB = r.get(RUB).json()
-        rep = f"1 USD = {dataUSD['conversion_rates']['RUB']} RUB" + '\n' \
-              f"1 EUR = {dataEUR['conversion_rates']['RUB']} RUB" + '\n' \
-              f"1 GBP = {dataGBP['conversion_rates']['RUB']} RUB" + '\n' \
-              f"1 RUB = {dataRUB['conversion_rates']['KRW']} KRW" + '\n' \
-              f"1 RUB = {dataRUB['conversion_rates']['MNT']} MNT"
+        rep = f"1 USD 🇺🇲 = {dataUSD['conversion_rates']['RUB']} RUB 🇷🇺" + '\n' \
+              f"1 EUR 🇪🇺 = {dataEUR['conversion_rates']['RUB']} RUB 🇷🇺" + '\n' \
+              f"1 GBP 🇬🇧 = {dataGBP['conversion_rates']['RUB']} RUB 🇷🇺" + '\n\n' \
+              f"1 RUB 🇷🇺 = {dataRUB['conversion_rates']['KZT']} KZT 🇰🇿" + '\n' \
+              f"1 RUB 🇷🇺 = {dataRUB['conversion_rates']['MNT']} MNT 🇲🇳"
         bot.reply_to(msg, rep)
-
-    # news
-
-    if msg.text == 'Новости 💀':
-        dot = '\u25CF'
-        message = 'Главные новости на сегодня:' + '\n' * 2
-        url = 'https://newsapi.org/v2/top-headlines?country=ru&apiKey=c86169d8dfb14848a46619b9f6d4006a'
-        data = r.get(url).json()
-        for i in range(10):
-            message += f"{dot} {data['articles'][i]['title']}\n{data['articles'][i]['url']}" + '\n' * 2
-        bot.reply_to(msg, message)
 
     # PIGGIE
 
@@ -169,5 +167,6 @@ def actions(msg):
         f = open(pig, 'rb')
         bot.send_photo(msg.chat.id, f)
         f.close()
+
 
 bot.infinity_polling()
